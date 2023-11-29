bbfb2dced8f7f4e72869e8ac0465fd7374fb2205
clownmoney <32284257+clownmoney@users.noreply.github.com>
2023-11-29 12:20:07 -0600

    0

--- server/routes/Auth.js
+++ server/routes/Auth.js
@@ -1,27 +1,41 @@
 const express = require("express");
 const router = express.Router();
 const { hashPassword, comparePassword } = require("../util/helpers");
-const User = require("../models").Users;
+const { Op } = require("sequelize");
+const { Users } = require("../models");
 
 router.post("/register", async (req, res) => {
-	console.log(req.body);
-	const { Username, Email, Password, FirstName, LastName } = req.body;
+	let { Username, Email, Password, FirstName, LastName } = req.body;
 
-	// Check if username or email already exists
-	const user = await User.findOne({ $or: [{ Username: Username }, { Email: Email }] });
-	if (user) {
-		return res.status(400).json({ message: "Username or email already taken" });
-	} else {
+	try {
+		// Force email and username to lowercase
+		Username = Username.toLowerCase();
+		Email = Email.toLowerCase();
+
+		// Check if username or email already exists
+		const existingUser = await Users.findOne({
+			where: {
+				[Op.or]: [{ Username: Username }, { Email: Email }],
+			},
+		});
+
+		if (existingUser) {
+			return res.status(400).json({ message: "Username or email already taken" });
+		}
+
 		// Create new user
-		const newUser = new User({
+		const newUser = await Users.create({
 			Username,
 			Email,
 			Password: hashPassword(Password),
 			FirstName,
 			LastName,
 		});
-		await newUser.save();
-		return res.status(201).json({ message: "User created" });
+
+		return res.status(201).json({ message: "User created", user: newUser });
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json({ message: "Internal Server Error" });
 	}
 });
 
